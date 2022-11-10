017d50e98b73dbcf83faabcf43a99caf03b371fe
jhan daza <jhand@keoworld.com>
2022-11-10 14:07:09 -0500

    updates

--- jenkins/scripts/staging.sh
+++ jenkins/scripts/staging.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
-npm install
-npm run build
-npm i -g firebase-tools@latest
-firebase -P dev target:apply hosting dev_target per-sider-qa --debug
-firebase deploy -P dev --only hosting:dev_target  #$FIREBASE_TOKEN --debug
+
+npm run staging
+
+npm install -g firebase-tools
+
+firebase deploy -P staging --token "$FIREBASE_DEPLOY_TOKEN"
+
+
+
+#npm install
+#npm run build
+#npm i -g firebase-tools@latest
+#firebase -P dev target:apply hosting dev_target per-sider-qa --debug
+#firebase deploy -P dev --only hosting:dev_target  #$FIREBASE_TOKEN --debug
